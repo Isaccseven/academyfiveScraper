f64265e934886abca29c33db2ea85aadc5de8e02
luca <luca-henn@t-online.de>
2022-08-14 15:06:24 +0200

    change display text

--- src/main/java/de/lucahenn/academyfive/controller/AcademyFiveController.java
+++ src/main/java/de/lucahenn/academyfive/controller/AcademyFiveController.java
@@ -18,7 +18,7 @@ public class AcademyFiveController {
 
     @GetMapping
     private String index() {
-        return "Welcome to ASW Academy Five API, the path you're searching for is /api/v1/grades :)";
+        return "Welcome to the Community API for the ASW, the path you're searching for is /api/v1/grades :)";
     }
 
     @PostMapping("/api/v1/grades")
