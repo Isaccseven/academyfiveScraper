c765c75f283cf27a8249d26978bae8687ceb5585
luca <luca-henn@t-online.de>
2022-09-12 23:13:33 +0200

    add Grade Model add GradeList Model refactor GradeService

---
+++ src/main/java/de/lucahenn/academyfive/model/GradeList.java
@@ -0,0 +1,12 @@
+package de.lucahenn.academyfive.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class GradeList {
+    private List<Grade> gradeList;
+}
--- src/main/java/de/lucahenn/academyfive/service/GradeService.java
+++ src/main/java/de/lucahenn/academyfive/service/GradeService.java
@@ -1,13 +1,14 @@
 package de.lucahenn.academyfive.service;
 
 import de.lucahenn.academyfive.model.Grade;
+import de.lucahenn.academyfive.model.GradeList;
 import org.springframework.stereotype.Service;
 
 import de.lucahenn.academyfive.constant.GradeConstants;
 import de.lucahenn.academyfive.model.Login;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 import org.jsoup.*;
 import org.jsoup.Connection.Response;
@@ -31,14 +32,15 @@ public class GradeService {
         return null;
     }
 
-    public ArrayList<Grade> returnGrades(Login login) {
+    public GradeList returnGrades(Login login) {
         try {
             Document doc = Jsoup.connect(GradeConstants.GRADE_URL.getConstant())
-                    .cookies(returnLoginResponse(login).cookies())
+                    .cookies(Objects.requireNonNull(returnLoginResponse(login)).cookies())
                     .post();
             ArrayList<Grade> selectedObjects = new ArrayList<>();
+            GradeList gradeList = new GradeList();
             doc.select(
-                    "#tabs-content-0 > div > div > div > div > div.panel-body > div > table > tbody > tr[style=font-weight: bold]")
+                    "#tabs-content-0 > div > div > div > div > div.panel-body > div > table > tbody > tr")
                     .forEach(element -> {
                         ArrayList<String> list = new ArrayList<String>();
                         Grade grade = new Grade();
@@ -48,7 +50,8 @@ public class GradeService {
                         });
                         selectedObjects.add(grade);
                     });
-            return selectedObjects;
+            gradeList.setGradeList(selectedObjects);
+            return gradeList;
         } catch (IOException e) {
             e.printStackTrace();
         }
